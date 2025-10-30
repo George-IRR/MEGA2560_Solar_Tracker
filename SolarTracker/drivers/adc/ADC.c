f9de84d46328d8d079aee32c8e24f38b3070ae37
George Radu <georgeradu169@gmail.com>
2025-10-30 20:04:14 +0200

    fix(ADC): Add support for ADC channels 8-15
    Updated ADC_read to handle channels 8-15 by setting or clearing the MUX5 bit in ADCSRB

--- SolarTracker/drivers/adc/ADC.c
+++ SolarTracker/drivers/adc/ADC.c
@@ -1,22 +1,27 @@
-/*
- * ADC.cpp
- *
- * Created: 9/26/2025 9:23:13 PM
- *  Author: George
- */ 
-
 #include <avr/io.h>
 
 void ADC_init(void){
-	ADMUX = (1<<REFS0); //Reference voltage 5V, ADC channel 0
-	
-	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1); //Enable ADC
+	ADMUX = (1<<REFS0); // Reference voltage AVCC (5V)
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1); // Enable ADC, prescaler 64
+	ADCSRB = 0; // Initialize ADCSRB
 }
 
-uint16_t ADC_read(uint8_t channel)
-{
-	ADMUX = (ADMUX & 0b11110000) | (channel & 0b00001111);
-	ADCSRA |= (1<<ADSC); //ADC Start Conversion
-	while(ADCSRA & (1<<ADSC)); //wait until ADC reading is complete to perform the next one
+uint16_t ADC_read(uint8_t channel){
+	// Mask to keep only the lower 3 bits for ADMUX (0-7)
+	ADMUX = (ADMUX & 0xE0) | (channel & 0x07);
+	
+	// Set or clear MUX5 bit in ADCSRB based on channel
+	if (channel >= 8) {
+		ADCSRB |= (1 << MUX5);  // Set MUX5 for channels 8-15
+		} else {
+		ADCSRB &= ~(1 << MUX5); // Clear MUX5 for channels 0-7
+	}
+	
+	// Start conversion
+	ADCSRA |= (1<<ADSC);
+	
+	// Wait until conversion is complete
+	while(ADCSRA & (1<<ADSC));
+	
 	return ADC;
 }
